579cde785053057fef9e413205f651c4ef474d39
ashwaltonQT <136596762+ashwaltonQT@users.noreply.github.com>
2024-10-15 09:04:35 +0000

    updated QString argument

--- QMLModules_Section3/main.cpp
+++ QMLModules_Section3/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.addImportPath(QString("%0/modules").arg(QGuiApplication::applicationDirPath()));
+    engine.addImportPath(QString("%1/modules").arg(QGuiApplication::applicationDirPath()));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
